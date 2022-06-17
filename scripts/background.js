b94d93dfbffd4ae0c7940ea12d1efc6dfe680f62
captcha781 <bhuvanesh19112001@gmail.com>
2022-06-17 09:50:13 +0530

    Primary lander completed needs css to be fixed for landscape orientation

---
+++ scripts/background.js
@@ -0,0 +1,40 @@
+let pri = document.getElementById("pri_head");
+
+let head_arr = ["Welcome,", "Swagata,", "Swaghatham,","Swaghat he,", "Bienvenu,", "Bienvenidos,", "أهلا بك,"];
+pri.innerHTML = head_arr[0]
+let j = 1;
+let i = 2;
+setInterval(() => {
+    if (i == 6){
+        i = 1;
+    }
+    document.getElementById("primary").style.transitionDelay = "1000ms";
+    document.getElementById("primary").style.backgroundImage = `url('/assets/mains/main-bg${i}.jpg')`;
+    document.getElementById("primary").style.transitionDuration = "1000ms";
+    document.getElementById("primary").style.transitionTimingFunction = "ease";
+    console.log(`url('/assets/mains/main-bg${i}.jpg')`);
+    i += 1
+    pri.innerHTML = '';
+    comer();
+},5000);
+
+const comer = () => {
+    if (j>= head_arr.length){
+        j = 0
+    }
+    let k = head_arr[j].split("")
+    let p =1;
+    pri.innerHTML = k[0]
+    setInterval(() => {
+        if(!k[p]){
+            k[p]=""
+        }
+        pri.innerHTML += k[p]
+        p += 1
+    },300);
+    
+    j+=1
+}
+
+
+
--- scripts/icon.js
+++ scripts/icon.js
@@ -1,10 +1,10 @@
 
 document.getElementById("openIcon").addEventListener("click", e => {
 	document.getElementById("menulist").style.left = "0";
-	document.getElementById("menulist").style.transitionDuration = "200ms"
+	document.getElementById("menulist").style.transitionDuration = "350ms"
 })
 
 document.getElementById("closeIcon").addEventListener("click", e => {
 	document.getElementById("menulist").style.left = "130%";
-	document.getElementById("menulist").style.transitionDuration = "200ms"
+	document.getElementById("menulist").style.transitionDuration = "350ms"
 })
